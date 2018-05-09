c5775c3ac438eae55d6c5b111dafbf1cc05b98cd
GaborMartin <gabormartin94@gmail.com>
2018-05-09 17:25:41 +0200

    Implemented PlazasServlet, deleted ProfileServlet

---
+++ src/main/java/com/codecool/web/servlet/PlazasServlet.java
@@ -0,0 +1,36 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.PlazaDao;
+import com.codecool.web.dao.database.DatabasePlazaImpl;
+import com.codecool.web.model.Plaza;
+import com.codecool.web.service.PlazaService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimplePlazaService;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+
+@WebServlet("/protected/plazas")
+public class PlazasServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            PlazaDao plazaDao = new DatabasePlazaImpl(connection);
+            PlazaService plazaService = new SimplePlazaService(plazaDao);
+
+            List<Plaza> plazas = plazaService.getAll();
+
+            sendMessage(resp, HttpServletResponse.SC_OK, plazas);
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+        }
+    }
+}
