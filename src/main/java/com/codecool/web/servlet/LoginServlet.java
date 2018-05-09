148fc6e3a3304e9b163fd80968d65f4239bbb843
GaborMartin <gabormartin94@gmail.com>
2018-05-09 17:25:20 +0200

    Deleted unnecessary imports

--- src/main/java/com/codecool/web/servlet/LoginServlet.java
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -1,7 +1,7 @@
 package com.codecool.web.servlet;
 
 import com.codecool.web.dao.UserDao;
-import com.codecool.web.dao.database.DatabaseUserDao;
+import com.codecool.web.dao.database.DatabaseUserImpl;
 import com.codecool.web.model.User;
 import com.codecool.web.service.LoginService;
 import com.codecool.web.service.exception.ServiceException;
@@ -20,7 +20,7 @@ public final class LoginServlet extends AbstractServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
-            UserDao userDao = new DatabaseUserDao(connection);
+            UserDao userDao = new DatabaseUserImpl(connection);
             LoginService loginService = new SimpleLoginService(userDao);
 
             String email = req.getParameter("email");
