7190a36132008f0d155084cb54da8ee309701714
GaborMartin <gabormartin94@gmail.com>
2018-05-10 11:47:45 +0200

    Implemented basic shops.js

---
+++ src/main/webapp/shops.js
@@ -0,0 +1,60 @@
+function onShopsResponse() {
+    const text = this.responseText;
+    const shops = JSON.parse(text);
+    const shopsContentDivEl = document.getElementById('shops-content');
+
+    const hEl = document.createElement('h3');
+    hEl.textContent = "Shops in plaza";
+
+    while (shopsContentDivEl.firstChild) {
+        shopsContentDivEl.removeChild(shopsContentDivEl.firstChild);
+    }
+
+    const ulEl = document.createElement('ul');
+
+    for (let i = 0; i < shops.length; i++) {
+        const shop = shops[i];
+
+        const spanEl = document.createElement('span');
+        spanEl.setAttribute('id', shop.id);
+        spanEl.textContent = shop.name;
+
+        //spanEl.addEventListener('click', onShopClicked);
+
+        const liEl = document.createElement('li');
+        liEl.append(spanEl);
+        ulEl.append(liEl);
+    }
+
+    shopsContentDivEl.append(hEl);
+    shopsContentDivEl.append(ulEl);
+}
+/*
+function onShopsLoad() {
+    const plazaId = this.dataset.plazaId;
+
+    const params = new URLSearchParams();
+    params.append('id', plazaId);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onShopsResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/shops');
+    xhr.send();
+} */
+
+function onPlazaClicked() {
+    clearMessages();
+    showContents(['profile-content', 'logout-content', 'shops-content', 'back-to-plazas-content']);
+
+    const plazaId = this.dataset.plazaId;
+
+    const params = new URLSearchParams();
+    params.append('id', plazaId);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onShopsResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/shops?' + params.toString());
+    xhr.send();
+}
