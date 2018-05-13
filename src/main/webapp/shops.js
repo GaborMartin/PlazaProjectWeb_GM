78e0fc04e80e9d1c844a49f0d3b5df3c5270b7c3
GaborMartin <gabormartin94@gmail.com>
2018-05-13 15:33:13 +0200

    Fixed recreation of plazas' list after logout and relogin

--- src/main/webapp/shops.js
+++ src/main/webapp/shops.js
@@ -1,34 +1,41 @@
+shopsContentDivEl;
 function onShopsResponse() {
-    const text = this.responseText;
-    const shops = JSON.parse(text);
-    const shopsContentDivEl = document.getElementById('shops-content');
+    clearMessages();
+    if (this.status === OK) {
+        const text = this.responseText;
+        const shops = JSON.parse(text);
+        const shopsContentDivEl = document.getElementById('shops-content');
 
-    const hEl = document.createElement('h3');
-    hEl.textContent = "Shops in plaza";
+        const hEl = document.createElement('h3');
+        hEl.textContent = "Shops in plaza";
 
-    while (shopsContentDivEl.firstChild) {
-        shopsContentDivEl.removeChild(shopsContentDivEl.firstChild);
+        while (shopsContentDivEl.firstChild) {
+            shopsContentDivEl.removeChild(shopsContentDivEl.firstChild);
+        }
+
+        const ulEl = document.createElement('ul');
+
+        for (let i = 0; i < shops.length; i++) {
+            const shop = shops[i];
+
+            const spanEl = document.createElement('span');
+            spanEl.setAttribute('id', shop.id);
+            spanEl.textContent = shop.name;
+            spanEl.dataset.shopId = shop.id;
+
+            spanEl.addEventListener('click', onShopClicked);
+
+            const liEl = document.createElement('li');
+            liEl.append(spanEl);
+            ulEl.append(liEl);
+        }
+
+            shopsContentDivEl.append(hEl);
+            shopsContentDivEl.append(ulEl);
+    } else {
+        removeAllChildren(shopsContentDivEl);
+        onOtherResponse(shopsContentDivEl, this);
     }
-
-    const ulEl = document.createElement('ul');
-
-    for (let i = 0; i < shops.length; i++) {
-        const shop = shops[i];
-
-        const spanEl = document.createElement('span');
-        spanEl.setAttribute('id', shop.id);
-        spanEl.textContent = shop.name;
-        spanEl.dataset.shopId = shop.id;
-
-        spanEl.addEventListener('click', onShopClicked);
-
-        const liEl = document.createElement('li');
-        liEl.append(spanEl);
-        ulEl.append(liEl);
-    }
-
-    shopsContentDivEl.append(hEl);
-    shopsContentDivEl.append(ulEl);
 }
 /*
 function onShopsLoad() {
